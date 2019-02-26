b07eaa796853048bb2647ed9eaf3ccd02b32d7ea
Taark <taark@scryptmail.com>
2019-02-26 21:55:29 +0300

    hz

--- tables.php
+++ tables.php
@@ -6,46 +6,80 @@
  * Time: 16:38
  */
 
-
-function getHashByString($string){
+function getHashByString($string) {
 	$length = strlen($string) - 1;
-	$hash   = 0;
+	$hash = 0;
 
-	for ($i=0 ; $i < strlen($string); $i++ ) { 
+	for ($i = 0; $i < strlen($string); $i++) {
 		$hash += ord($string[$i]);
 	}
 	return $hash;
 }
 
+function findValueInArray($value, $array) {
+
+	$hash = getHashByString($value);
+
+	if (empty($array[$hash])) {
+		return null;
+	}
+
+	if ($array[$hash] == $value) {
+		return $hash;
+	}
+
+	while (isset($array[$hash])) {
+		$hash++;
+		if ($array[$hash] == $value) {
+			return $hash;
+		}
+	}
+}
+
 $list = json_decode(file_get_contents("list.json"), true);
 $list = $list["items"];
 
-$example = "z";
-
-
-
-
+$example = 'x';
 
 $itemsHash = [];
 
 foreach ($list as $item) {
-    $itemsHash[getHashByString($item)] = $item;
+	$hash = getHashByString($item);
+	$hashIsSet = isset($itemsHash[$hash]);
+
+	if (!$hashIsSet) {
+		$itemsHash[$hash] = $item;
+		continue;
+	}
+
+	while ($hashIsSet) {
+		$hash++;
+
+		if (!isset($itemsHash[$hash])) {
+			$itemsHash[$hash] = $item;
+			$hashIsSet = false;
+		} else {
+			if ($itemsHash[$hash] == $item) {
+				break;
+			}
+		}
+	}
 }
 
-var_dump($itemsHash);
-var_dump($itemsHash[getHashByString($example)]);
-
-
+print_r(json_encode($itemsHash));
+print("\n");
 
 $itemsSimple = [];
 
 foreach ($list as $item) {
-    if(in_array($item, $itemsSimple)) continue;
+	if (in_array($item, $itemsSimple)) {
+		continue;
+	}
 
-    $itemsSimple[] = $item;
+	$itemsSimple[] = $item;
 }
 
-
-var_dump($itemsSimple);
-var_dump($itemsSimple[array_search($example, $itemsSimple)]);
-
+print(json_encode($itemsSimple));
+print("\n");
+print(json_encode($itemsSimple[array_search($example, $itemsSimple)]));
+print("\n");
