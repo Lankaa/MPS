f3936b87d0bf413f41ba4e10ee085a16b506df99
Taark <taark@scryptmail.com>
2019-02-28 01:59:51 +0300

    add simple parser

--- 3/analyzer.php
+++ 3/analyzer.php
@@ -1,21 +1,54 @@
 <?php
 
-/**
- *
- */
 class Analyzer {
 	private $code;
 
 	private const KEYWORD = ['main', 'for'];
+	private const VARIABLES = 'int.(.*);';
+	private const SEPARATOR = ['\(', '\)', '\{', '\}', '\,', '\;', '\+', '\<', '\>', '\=', '\*'];
 
 	function __construct($code) {
 		$this->code = $code;
 	}
 
-	public function getKeyword() {
-		$pattern = join('|', self::KEYWORD);
-		preg_match("/$pattern/", $this->code, $matches);
+	private function prepareResponse($response) {
+		$preparedResponse = [];
 
+		foreach ($response as $value) {
+			if (is_array($value)) {
+				foreach ($value as $invalue) {
+					$preparedResponse[] = $invalue;
+				}
+			} else {
+				$preparedResponse[] = $invalue;
+			}
+		}
+		return $preparedResponse;
+	}
+
+	private function analyze($pattern) {
+		preg_match_all("/$pattern/m", $this->code, $matches);
 		return $matches;
 	}
+
+	public function getKeyword() {
+		return $this->prepareResponse($this->analyze(join('|', self::KEYWORD)));
+	}
+
+	public function getSeparator() {
+		return $this->prepareResponse($this->analyze(join('|', self::SEPARATOR)));
+	}
+
+	public function getVariables() {
+		$tmp = $this->analyze(self::VARIABLES)[1];
+		$variables = [];
+
+		foreach ($tmp as $var) {
+			$variables[] = preg_split('/,/', $var);
+		}
+
+		return $this->prepareResponse($variables);
+
+	}
+
 }
--- 3/main.php
+++ 3/main.php
@@ -4,3 +4,16 @@ require_once 'analyzer.php';
 $code = file_get_contents('main.c');
 
 $analyzer = new Analyzer($code);
+
+$keyword = $analyzer->getKeyword();
+$variables = $analyzer->getVariables();
+$separator = $analyzer->getSeparator();
+
+print_r(json_encode(['keyword' => $keyword]));
+print("\n");
+
+print_r(json_encode(['variables' => $variables]));
+print("\n");
+
+print_r(json_encode(['separator' => $separator]));
+print("\n");
